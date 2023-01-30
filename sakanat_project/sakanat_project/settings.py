77f91e5606f51b4bd7b0a174862144d3cd467e40
Aqeed Ghazal <aqeedghazal@gmail.com>
2023-01-30 19:12:29 +0200

    Change urls.py , setting.py

--- sakanat_project/sakanat_project/settings.py
+++ sakanat_project/sakanat_project/settings.py
@@ -119,3 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'files')
+MEDIA_URL = '/files/'
